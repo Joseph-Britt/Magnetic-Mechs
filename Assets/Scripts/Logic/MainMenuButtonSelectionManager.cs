ff14a593e0dc5007b5d5f7b0a547418e4d8b3a47
Rishi Aitha <rishi.aitha@gmail.com>
2025-11-09 14:12:00 -0500

    updated button navigation

--- Assets/Scripts/Logic/MainMenuButtonSelectionManager.cs
+++ Assets/Scripts/Logic/MainMenuButtonSelectionManager.cs
@@ -11,19 +11,31 @@ public class MainMenuButtonSelectionManager : MonoBehaviour
     //manages selecting buttons in the menu and pause screen
     [Header("Game Objects")]
     public Transform buttonParent;
+    public Transform page1Parent;
+    public Transform page2Parent;
     [Header("Variables")]
     public List<GameObject> buttons;
+    public List<GameObject> page1Buttons;
+    public List<GameObject> page2Buttons;
     [Header("Timers")]
     private float delay = .02f;
     private float readyToChange = 0f;
     public int currentSelection = 0;
+    public int currentPage = 0;
 
     private void Awake()
     {
-        foreach (Transform child in buttonParent) {
+        foreach (Transform child in page1Parent)
+        {
             GameObject button = child.gameObject;
-            buttons.Add(button);
+            page1Buttons.Add(button);
         }
+        foreach (Transform child in page2Parent)
+        {
+            GameObject button = child.gameObject;
+            page2Buttons.Add(button);
+        }
+        buttons = page1Buttons;
         GameObject savedVariablesObject = GameObject.FindGameObjectWithTag("MultiSceneVariables");
         SetButtonSize(currentSelection);
     }
@@ -38,6 +50,13 @@ public class MainMenuButtonSelectionManager : MonoBehaviour
                 currentSelection += 1;
                 if(currentSelection >= buttons.Count)
                 {
+                    if (currentPage == 0)
+                    {
+                        currentPage = 1;
+                        buttons = page2Buttons;
+                        page1Parent.gameObject.SetActive(false);
+                        page2Parent.gameObject.SetActive(true);
+                    }
                     currentSelection = 0;
                 }
             }
@@ -46,6 +65,13 @@ public class MainMenuButtonSelectionManager : MonoBehaviour
                 currentSelection -= 1;
                 if (currentSelection < 0)
                 {
+                    if (currentPage == 1)
+                    {
+                        currentPage = 0;
+                        buttons = page1Buttons;
+                        page1Parent.gameObject.SetActive(true);
+                        page2Parent.gameObject.SetActive(false);
+                    }
                     currentSelection = buttons.Count - 1;
                 }
             }
