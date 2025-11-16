95b4b486e3e026ff69c5112d4499628802a7f128
seesee7164 <checkeredsage54@gmail.com>
2025-11-16 13:20:43 -0500

    Making adjustment/updates to new tower level
    Making adjustment/updates to new tower level

---
+++ Assets/LavaFloorScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class LavaFloorScript : MonoBehaviour
+{
+    [Header("variables")]
+    private float speed = 1.5f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        transform.position += Vector3.up * speed * Time.fixedDeltaTime;
+    }
+}
